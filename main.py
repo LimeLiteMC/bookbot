3aa45a707b77d31cd76e4ac6bd2a4ede727d930f
Nathan Stevenson <nathanstevenson@Mac.lan>
2025-11-02 21:11:27 -0700

    Created functions get_book_text and main in main.py.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(file_path):
+    file = ""
+    with open(file_path) as book:
+        file = book.read()
+    return file
+
+def main():
+    book = get_book_text("./books/frankenstein.txt")
+    print(book)
+
+main()
